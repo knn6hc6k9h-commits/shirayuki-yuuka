298640c1de2c7e0c7152ab8df27d2cfa91e252e2
knn6hc6k9h-commits <knn6hc6k9h@privaterelay.appleid.com>
2026-09-13 19:04:50 +0900

    Rebuild word meaning SHIORI wrapper

--- source/shiori_callname.c
+++ source/shiori_callname.c
@@ -1,4 +1,4 @@
-/* Top-level SHIORI wrapper: call-name handling + editable menu labels. */
+/* Top-level SHIORI wrapper: call-name handling + editable menu labels + word meanings. */
 #include "shiori_callname_part1.inc"
 #include "shiori_callname_part2.inc"
 #include "shiori_callname_part3.inc"
